f5c6a2952cbd24253e5632bd323163af283d99b1
Iann-rst <iannrst11@gmail.com>
2022-11-30 19:54:03 -0300

    feat: Chamar o caso de uso de criar um usuário

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,10 +3,21 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private createUserUseCase: CreateUserUseCase) {
+    // constructor
+  }
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
+    const { name, email } = request.body;
+
+    try {
+      const user = this.createUserUseCase.execute({ name, email });
+
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: "Usuário ja cadastrado!" });
+    }
   }
 }
 
