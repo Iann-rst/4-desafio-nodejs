a4ec55e984ca58f7cb0d70c7b3a2d501f0870017
Iann-rst <iannrst11@gmail.com>
2022-11-30 21:00:24 -0300

    feat: Executar a função do ShowUserProfileUseCase

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,19 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const id = user_id.toString();
+
+      const user = this.showUserProfileUseCase.execute({ user_id: id });
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
