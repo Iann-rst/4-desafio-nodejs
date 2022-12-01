818551def607a1eb608196a28b400c56ed6ec5a7
Iann-rst <iannrst11@gmail.com>
2022-11-30 21:45:35 -0300

    fix: CreateUserController

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,15 +3,12 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {
-    // constructor
-  }
+  constructor(private createUserUseCase: CreateUserUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
-
     try {
       const { name, email } = request.body;
+
       const user = this.createUserUseCase.execute({ name, email });
 
       return response.status(201).json(user);
